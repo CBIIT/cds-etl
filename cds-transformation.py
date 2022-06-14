6d0dfeea4d87ae38de42432f93dc73b68d41ba58
Fuyuan Wang <wfy199719971997@gmail.com>
2022-06-14 09:39:34 -0400

    Update to extract data from new excel file

--- cds-transformation.py
+++ cds-transformation.py
@@ -9,36 +9,52 @@ args = parser.parse_args()
 config = args.config_file
 
 def extract_data(df, model, df_list, node):
+    new_df = pd.DataFrame()
     for cds_df in df_list:
         for property in model['Nodes'][node]['Props']:
             for col in cds_df.columns:
                 col_name = col.replace(" ", "_").lower()
                 if col_name in property or property in col_name:
                     #if len(cds_df) > len(df):
-                    df[property] = cds_df[col]
-            if property == 'file_name' and 'filename' in df.keys():
-                df['file_name'] = cds_df['filename']
-            if property == 'file_type' and 'filetype' in df.keys():
-                df['file_type'] = cds_df['filetype']
+                    new_df[property] = cds_df[col]
+    
+    for property in new_df.keys():
+        if property in df.keys():
+            df = df.join(new_df.set_index(property), on=property)
             df['type'] = [node] * len(df)
+            return df
+    df = pd.concat([df, new_df], axis=1)
+    df['type'] = [node] * len(df)
     return df
-
 
 #Add acl to file node
-def add_properties(file_name, df, cds_df):
+def add_properties(file_name, df, cds_df, dataframe_name):
     props = [
-        {'node':'file', 'new_property':'acl', 'new_value': cds_df['acl']},
-        {'node':'file', 'new_property':'file_id', 'new_value': cds_df['GUID']},
-        {'node':'file', 'new_property':'file_description', 'new_value': cds_df['title']},
-        {'node':'genomic_info', 'new_property':'file.file_id', 'new_value': cds_df['GUID']},
-        {'node':'file', 'new_property':'sample.sample_id', 'new_value': cds_df['sample_id']},
-        {'node':'genomic_info', 'new_property':'platform', 'new_value': cds_df['library_platform']},
+        {'node':'file', 'new_property':'acl', 'new_value': 'acl', 'dataframe_name': 'CDS_Manifest_df'},
+        {'node':'file', 'new_property':'file_id', 'new_value': 'GUID', 'dataframe_name': 'CDS_Manifest_df'},
+        {'node':'file', 'new_property':'sample.sample_id', 'new_value': 'sample_id', 'dataframe_name': 'CDS_Manifest_df'},
+        {'node':'genomic_info', 'new_property':'library_id', 'new_value': 'library_id', 'dataframe_name': 'CDS_Manifest_df'},
+        {'node':'genomic_info', 'new_property':'file.file_id', 'new_value': 'GUID', 'dataframe_name': 'CDS_Manifest_df'},
+        {'node':'sample', 'new_property':'sample_id', 'new_value': 'Sample ID', 'dataframe_name': 'CGC_CDS_Explorer_df'},
+        {'node':'sample', 'new_property':'participant.participant_id', 'new_value': 'Participant ID', 'dataframe_name': 'CGC_CDS_Explorer_df'},
+        {'node':'participant', 'new_property':'study.phs_accession', 'new_value': 'phs_accession', 'dataframe_name': 'Study_df'}
+
     ]
+    new_df = pd.DataFrame()
     for property in props:
-        if property['node'] == file_name:
-             df[property['new_property']] = property['new_value']
+        if property['node'] == file_name and property['dataframe_name'] == dataframe_name:
+             new_df[property['new_property']] = cds_df[property['new_value']]
     
+    for property in new_df.keys():
+        if property in df.keys():
+            df = df.join(new_df.set_index(property), on = property)
+            df = df.drop_duplicates()
+            return df
+    if len(new_df) != len(df):
+        new_df = new_df[0:len(df)]
+    df = pd.concat([df, new_df], axis=1)
     return df
+
 #Remove node
 def remove_node(df, file_name):
     nodes = [
@@ -108,18 +124,19 @@ df_dict = {}
 for node in model['Nodes']:
     #print(node)
     df = pd.DataFrame()
-    df_list = [Sample, File, Genomic_Info, CDS_Manifest_df, CGC_CDS_Explorer_df, SRA_Run_Selector_df, Study_df]
+    df_list = [File, Genomic_Info, CDS_Manifest_df, CGC_CDS_Explorer_df, SRA_Run_Selector_df, Study_df, Sample]
     df = extract_data(df, model, df_list, node)
-    df = add_properties(node, df, CDS_Manifest_df)
+    
     df = remove_node(df, node)
     #participant should only come from Participant sheet
     df = extract_data(df, model, [Participant], node)
+    df = add_properties(node, df, CDS_Manifest_df, 'CDS_Manifest_df')
+    df = add_properties(node, df, CGC_CDS_Explorer_df, 'CGC_CDS_Explorer_df')
+    df = add_properties(node, df, Study_df, 'Study_df')
+
     if len(df) > 0:
         df_dict[node] = df
-
 #improve the data after extraction
-df_dict['sample']['participant.participant_id '] = CGC_CDS_Explorer_df['Participant ID']
-df_dict['participant']['study.phs_accession'] = df_dict['study']['phs_accession'][0:len(df_dict['participant'])]
 df_dict['study'] = df_dict['study'].drop(columns = ['size_of_data_being_uploaded', 'study_external_url'])
 df_dict['study'] =  df_dict['study'].drop_duplicates()
 for node in df_dict.keys():
