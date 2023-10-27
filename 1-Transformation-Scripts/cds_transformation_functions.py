58a36af61fecfd01760d52476101d9258a1ca138
Fuyuan Wang <60631776+wfy1997@users.noreply.github.com>
2023-10-27 12:47:34 -0400

    add condition statement to prevent download only excel file

--- 1-Transformation-Scripts/cds_transformation_functions.py
+++ 1-Transformation-Scripts/cds_transformation_functions.py
@@ -328,7 +328,8 @@ def download_from_s3(config, cds_log):
                 # If the path does not exist, then create the folder
                 os.mkdir(download_folder)
             file_key = os.path.join(download_folder, os.path.basename(key['Key']))
-            s3.download_file(config['S3_BUCKET'], key['Key'], file_key)
+            if key.endswith(".xlsx"):
+                s3.download_file(config['S3_BUCKET'], key['Key'], file_key)
 
 def combine_columns(df_dict, config, cds_log):
     for combine_node in config['COMBINE_COLUMN']:
